3c67e236e200a5f52f6f1fcd662fb25bdaa18945
Facundo Decunta <fdecunta@agro.uba.ar>
2026-09-14 09:39:44 -0300

    dont allow quote as separator

--- csvfill.c
+++ csvfill.c
@@ -56,7 +56,7 @@ main(int argc, char *argv[])
 			tbl2.idfield = readnum(optarg);
 			break;
 		case 'd':
-			if (strlen(optarg) > 1 || optarg[0] == '\0') {
+			if (strlen(optarg) > 1 || optarg[0] == '\0' || optarg[0] == '"') {
 				errx(EXIT_FAILURE, "illegal delimiter: %s", optarg);
 			} 
 			delim = optarg[0];
